43d5327c34c74078f6a4427997793210a1c5a5b9
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-06-14 01:17:58 -0300

    Inserts

--- relebrandoOperacoesSQL.sql
+++ relebrandoOperacoesSQL.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS `testes_estudo`;
 CREATE DATABASE IF NOT EXISTS `testes_estudo`;
 USE `testes_estudo`;
 
@@ -23,4 +24,27 @@ CREATE TABLE `roles_user_table`(
     PRIMARY KEY(`id`),
     FOREIGN KEY(`user_id`) REFERENCES `user_table`(`id`),
     FOREIGN KEY(`role_id`) REFERENCES `role_table`(`id`)
-)
+);
+
+INSERT INTO `user_table` (`name_user`, `email_user`) 
+VALUES 
+("Bruno", "bruno@gmail.com"),
+("Pedro", "Pedro@gmail.com"),
+("Rafaela", "Rafaela@gmail.com"),
+("Julia", "Julia@gmail.com");
+
+INSERT INTO `role_table` (`role_name`)
+VALUES
+("admin"),
+("user"),
+("diretoctor");
+
+INSERT INTO `roles_user_table` (`user_id`, `role_id`)
+VALUES 
+(1,1),
+(1,2),
+(1,3),
+(2,2),
+(3,2);
+
+SELECT * FROM `roles_user_table`;
